8e634fadedc0118efdb301366f5f63d7a599c852
ashsangi <143747653+ashsangi@users.noreply.github.com>
2023-11-23 23:02:48 -0500

    Delete ElevatorSimulator directory

--- ElevatorSimulator/src/main/java/com/main/elevatorsimulator/structure/elevators/ElevatorType.java
+++
@@ -1,7 +0,0 @@
-package com.main.elevatorsimulator.structure.elevators;
-/**
- * Enum representing different types of elevators.
- */
-public enum ElevatorType {
-    StandardElevator, ExpressElevator, FreightElevator, GlassElevator
-}
--- ElevatorSimulator/src/main/java/com/main/elevatorsimulator/structure/passengers/PassengerType.java
+++
@@ -1,8 +0,0 @@
-package com.main.elevatorsimulator.structure.passengers;
-
-/**
- * Enum representing different types of passengers.
- */
-public enum PassengerType {
-    Standard, VIP, Freight, Glass
-}
